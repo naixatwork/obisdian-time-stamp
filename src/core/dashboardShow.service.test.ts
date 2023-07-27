7019e33937061622ac0fd6b689e540a44cf56e4b
Mohammad Heydari <47766973+naixatwork@users.noreply.github.com>
2023-07-27 16:13:16 +0330

    feat(dashboardComponent): introduced testing

--- src/core/dashboardShow.service.test.ts
+++ src/core/dashboardShow.service.test.ts
@@ -2,17 +2,19 @@ import expect from "expect";
 import {container} from "tsyringe";
 import DashboardShowService from "./dashboardShow.service";
 
-describe('dashboard', () => {
+describe('DashboardShowService', () => {
 	let service = container.resolve(DashboardShowService);
 
 	beforeEach(() => {
 		service = container.resolve(DashboardShowService);
 	});
 
-	test('it should be defined', () => {
-		expect(service).toBeDefined();
+	test('it should notify and emit values through showApp$', (done) => {
+		service.showApp$.subscribe((value) => {
+			expect(value).toBe(null);
+			done();
+		});
+
+		service.notifyShowApp();
 	});
-
-	test('it should have #showEvent$ as ');
 });
-
--- src/dashboard/dashboard.component.tsx
+++ src/dashboard/dashboard.component.tsx
@@ -1,17 +1,17 @@
-import * as React from 'react'
+import * as React from 'react';
 import {useState} from "react";
 
 export const DashboardComponent = () => {
-    const [counter, setCounter] = useState(0)
+	const [counter, setCounter] = useState(0);
 
-    return (
-        <div data-testid="app">
-            <h1 className="text-3xl font-bold underline">{counter}</h1>
-            <button onClick={() => {
-                setCounter(prev => prev+1)
-            }}>
-                lol
-            </button>
-        </div>
-    )
-}
+	return (
+		<div data-testid="app">
+			<h1 className="text-3xl font-bold underline">{counter}</h1>
+			<button onClick={() => {
+				setCounter(prev => prev + 1);
+			}}>
+				lol
+			</button>
+		</div>
+	);
+};
