fdab4027dad35a2943c5be29eab4a4b9ed2f231e
Mohammad Heydari <47766973+naixatwork@users.noreply.github.com>
2023-07-28 07:03:56 +0330

    feat(icons): installed lucide icons

--- src/dashboard/dashboard.component.tsx
+++ src/dashboard/dashboard.component.tsx
@@ -1,12 +1,14 @@
 import * as React from 'react';
-import {useState} from "react";
+import {useState} from 'react';
+import {Camera} from "lucide-react";
 
 export const DashboardComponent = () => {
 	const [counter, setCounter] = useState(0);
 
 	return (
 		<div data-testid="app">
-			<h1 className="text-3xl font-bold underline">{counter}</h1>
+			<h1 className="tw-flex tw-bg-amber-50">{counter}</h1>
+			<Camera size={24}/>
 			<button onClick={() => {
 				setCounter(prev => prev + 1);
 			}}>
