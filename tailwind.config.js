37ff99eed0620e5456a419677674558f345e19dc
Mohammad Heydari <47766973+naixatwork@users.noreply.github.com>
2023-07-28 06:47:03 +0330

    fix(tailwind): disabled tailwind preflight

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{tsx,css}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+	content: ["./src/**/*.{tsx,css}"],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+	corePlugins: {
+		preflight: false,
+	}
+};
