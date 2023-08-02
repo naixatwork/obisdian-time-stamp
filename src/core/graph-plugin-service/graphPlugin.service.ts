65dfea25d5dd6ae4ecf7461c427ef1cc2905b198
Mohammad Heydari <47766973+naixatwork@users.noreply.github.com>
2023-08-02 10:16:47 +0330

    feat(graph): introduced query on markdownFiles according to graph files

--- src/core/graph-plugin-service/graphPlugin.service.ts
+++ src/core/graph-plugin-service/graphPlugin.service.ts
@@ -2,46 +2,58 @@ import {container, singleton} from "tsyringe";
 import MAIN_IDENTIFIERS from "../../shared/main.identifiers";
 import MainPlugin from "../../../main";
 import {fromPromise} from "rxjs/internal/observable/innerFrom";
-import {BehaviorSubject, Observable, takeUntil, tap} from "rxjs";
+import {BehaviorSubject, forkJoin, map, Observable, takeUntil} from "rxjs";
 import {GraphGroup, GraphSettings} from "./graphPlugin.types";
 import MarkdownFilesService from "../markdown-files-service/markdownFiles.service";
 import DashboardUnloadService from "../dashboard-unload-service/dashboardUnload.service";
+import * as _ from 'lodash';
 
 @singleton()
 export default class GraphPluginService {
-    #mainPlugin = container.resolve<MainPlugin>(MAIN_IDENTIFIERS.mainPlugin);
-    #dashboardUnloadService = container.resolve(DashboardUnloadService);
-    #markdownFilesService = container.resolve(MarkdownFilesService);
+	#mainPlugin = container.resolve<MainPlugin>(MAIN_IDENTIFIERS.mainPlugin);
+	#dashboardUnloadService = container.resolve(DashboardUnloadService);
+	#markdownFilesService = container.resolve(MarkdownFilesService);
 
-    #groupedMarkdownFiles$ = new BehaviorSubject<GraphGroup[]>([]);
+	#groupedMarkdownFiles$ = new BehaviorSubject<GraphGroup[]>([]);
 
-    constructor() {
-        this.#vaultFilesFilteredByGroups();
-        this.#groupedMarkdownFiles$.subscribe(console.log);
-    }
+	constructor() {
+		this.#groupedMarkdownFiles$.subscribe(console.log);
+	}
 
-    get #graphPluginSettings$(): Observable<GraphSettings> {
-        // @ts-expect-error
-        return fromPromise(this.#mainPlugin.app.internalPlugins.getPluginById("graph")?.loadData());
-    }
+	get #graphPluginSettings$(): Observable<GraphSettings> {
+		// @ts-expect-error
+		return fromPromise(this.#mainPlugin.app.internalPlugins.getPluginById("graph")?.loadData());
+	}
 
-    async #vaultFilesFilteredByGroups() {
-        this.#graphPluginSettings$
-            .pipe(
-                takeUntil(this.#dashboardUnloadService.dashboardUnload$),
-                tap((graphSettings) => {
-                    graphSettings.colorGroups.forEach((colorGroup) => {
-                        this.#markdownFilesService.getMarkdownFilesByName$(colorGroup.query).subscribe({
-                            next: (files) => {
-                                this.#groupedMarkdownFiles$.next([...this.#groupedMarkdownFiles$.value, {
-                                    group: colorGroup,
-                                    associatedFiles: files,
-                                    name: colorGroup.query
-                                }]);
-                            }
-                        });
-                    });
-                })
-            ).subscribe();
-    }
+	public filesCategoryByGroups(): Observable<GraphGroup[]> {
+		return forkJoin({
+			graphColorGroups: this.#graphPluginSettings$
+				.pipe(map((graphSettings) => graphSettings.colorGroups)),
+			markdownFiles: this.#markdownFilesService.markdownFiles$
+		}).pipe(
+			takeUntil(this.#dashboardUnloadService.dashboardUnload$),
+			map(({graphColorGroups, markdownFiles}) => {
+				const mutableMarkdownFiles = [...markdownFiles];
+				const filesCategoryByGroup: GraphGroup[] = [];
+				graphColorGroups.forEach((colorGroup) => {
+					filesCategoryByGroup.push({
+						group: colorGroup,
+						name: colorGroup.query,
+						associatedFiles: _.remove(mutableMarkdownFiles, ({name}) => name.includes(colorGroup.query))
+					});
+				});
+
+				filesCategoryByGroup.push({
+					group: {
+						query: 'ungrouped',
+						color: {a: 1, rgb: 120120120}
+					},
+					name: 'ungrouped',
+					associatedFiles: [...mutableMarkdownFiles]
+				});
+
+				return filesCategoryByGroup;
+			})
+		);
+	}
 }
--- src/core/markdown-files-service/markdownFiles.service.ts
+++ src/core/markdown-files-service/markdownFiles.service.ts
@@ -1,6 +1,5 @@
 import {container, singleton} from "tsyringe";
-import {BehaviorSubject, delayWhen, map, Observable, of, retryWhen, takeUntil, tap, timer} from "rxjs";
-import {TFile} from "obsidian";
+import {delayWhen, map, of, retryWhen, takeUntil, tap, timer} from "rxjs";
 import MainPlugin from "../../../main";
 import MAIN_IDENTIFIERS from "../../shared/main.identifiers";
 import DashboardUnloadService from "../dashboard-unload-service/dashboardUnload.service";
@@ -10,26 +9,8 @@ export default class MarkdownFilesService {
 	#mainPlugin = container.resolve<MainPlugin>(MAIN_IDENTIFIERS.mainPlugin);
 	#dashboardUnloadService = container.resolve(DashboardUnloadService);
 
-	#markdownFiles$ = new BehaviorSubject<TFile[]>([]);
-
-	constructor() {
-		this.#initializeMarkdownFiles();
-	}
-
-	public get markdownFiles$(): Observable<TFile[]> {
-		return this.#markdownFiles$.asObservable();
-	}
-
-	public getMarkdownFilesByName$(name: string): Observable<TFile[]> {
-		return this.#markdownFiles$.asObservable().pipe(
-			map((markdownFiles) => {
-				return markdownFiles.filter((file) => file.name.contains(name));
-			})
-		);
-	}
-
-	#initializeMarkdownFiles() {
-		of(this.#mainPlugin.app.vault.getMarkdownFiles())
+	public get markdownFiles$() {
+		return of(this.#mainPlugin.app.vault.getMarkdownFiles())
 			.pipe(
 				takeUntil(this.#dashboardUnloadService.dashboardUnload$),
 				map(() => {
@@ -46,10 +27,6 @@ export default class MarkdownFilesService {
 					tap(val => console.log(`there was no markdown files`)),
 					delayWhen(() => timer(300))
 				)),
-			).subscribe({
-			next: (files) => {
-				this.#markdownFiles$.next(files);
-			}
-		});
+			);
 	}
 }
